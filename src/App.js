098d5bcbff54017ae1843ea289c38f069ab22c60
thakkardhavalc <thakkardhavalc@gmail.com>
2023-12-29 14:58:37 -0700

    refactored the code by created new file as CounterButton and move code

--- src/App.js
+++ src/App.js
@@ -5,9 +5,7 @@ import Counter from './components/counter/Counter';
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
-      <Counter by={2}></Counter>
-      <Counter by={5}></Counter>
+      <Counter />
     </div>
   )
 }
--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,35 +1,34 @@
 import { useState } from 'react'
-import { PropTypes } from 'prop-types'
 import './Counter.css'
+import CounterButton from './CounterButton'
 
-export default function Counter({by}) {
+export default function Counter() {
 
-    const [count, setCount] = useState(0);
-    
-    function incrementCounterFunction() {
+    const [count, setCount] = useState(0)
+
+    function incrementCounterParentFunction(by) {
         setCount(count + by)
     }
 
-    function decrementCounterFunction() {
+    function decrementCounterParentFunction(by) {
         setCount(count - by)
     }
-    
-    
+
     return(
-        <div className="Counter">
-            <span className="count">{count}</span>
-            <div>
-                <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
-                <button className="counterButton" onClick={decrementCounterFunction}>-{by}</button>
-            </div>
+        <div>
+            <span className="totalCount">{count}</span>
+            <CounterButton by={1} 
+                incrementMethod={incrementCounterParentFunction} 
+                decrementMethod={decrementCounterParentFunction} 
+            />
+            <CounterButton by={2} 
+                incrementMethod={incrementCounterParentFunction} 
+                decrementMethod={decrementCounterParentFunction} 
+            />
+            <CounterButton by={5} 
+                incrementMethod={incrementCounterParentFunction} 
+                decrementMethod={decrementCounterParentFunction} 
+            />
         </div>
     )
-}
-
-Counter.propTypes = {
-    by: PropTypes.number
-}
-
-Counter.defaultProps = {
-    by: 1
 }
