6bd91185e72b7606c8d48d140898919318c7326f
thakkardhavalc <thakkardhavalc@gmail.com>
2023-12-29 14:21:04 -0700

    added counter code created new file under Counter folder

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
-import LearningComponent from './components/learning-examples/LearningComponent';
+import Counter from './components/counter/Counter';
+
 
 function App() {
   return (
     <div className="App">
-      <LearningComponent></LearningComponent>
+      <Counter></Counter>
+      <Counter by={2}></Counter>
+      <Counter by={5}></Counter>
     </div>
   )
 }
---
+++ src/components/counter/Counter.jsx
@@ -0,0 +1,35 @@
+import { useState } from 'react'
+import { PropTypes } from 'prop-types'
+import './Counter.css'
+
+export default function Counter({by}) {
+
+    const [count, setCount] = useState(0);
+    
+    function incrementCounterFunction() {
+        setCount(count + by)
+    }
+
+    function decrementCounterFunction() {
+        setCount(count - by)
+    }
+    
+    
+    return(
+        <div className="Counter">
+            <span className="count">{count}</span>
+            <div>
+                <button className="counterButton" onClick={incrementCounterFunction}>+{by}</button>
+                <button className="counterButton" onClick={decrementCounterFunction}>-{by}</button>
+            </div>
+        </div>
+    )
+}
+
+Counter.propTypes = {
+    by: PropTypes.number
+}
+
+Counter.defaultProps = {
+    by: 1
+}
